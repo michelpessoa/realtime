ab5a5e1e53fc34f909250ecda9457e53f1b810fc
Michel Pessoa Keursten <michelpessoa@gmail.com>
2020-09-21 11:29:07 -0300

    Update settings.py

--- realtime/settings.py
+++ realtime/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['192.168.1.8', '127.0.0.1', 'localhost', '192.168.1.9']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
